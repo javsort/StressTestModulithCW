a51f3f82efa6bdd8ab7120c27434a9b21036950f
javsort <javierortega336@gmail.com>
2024-11-16 15:12:05 +0100

    Test has been implemented, currently fixing variable usage for ID exchange & sessionId management

--- src/test/java/de/unistuttgart/t2/modulith/gatling_test/StressTestSimu.java
+++ src/test/java/de/unistuttgart/t2/modulith/gatling_test/StressTestSimu.java
@@ -1,14 +1,14 @@
 package de.unistuttgart.t2.modulith.gatling_test;
 
-import io.gatling.core.scenario.Simulation;
+//import io.gatling.core.scenario.Simulation;
 
-import static io.gatling.javaapi.core.CoreDsl.*;
+import io.gatling.javaapi.core.ChainBuilder; 
+import io.gatling.javaapi.core.ScenarioBuilder; 
+import io.gatling.javaapi.core.Simulation; 
+import io.gatling.javaapi.http.HttpProtocolBuilder; 
+import static io.gatling.javaapi.core.CoreDsl.*; 
 import static io.gatling.javaapi.http.HttpDsl.*;
 
-import java.util.concurrent.ThreadLocalRandom;
-
-import io.gatling.javaapi.core.*;
-import io.gatling.javaapi.http.*;
 
 /*
  * Coursework 1.C. - T2 - Modulith
@@ -20,40 +20,98 @@ public class StressTestSimu extends Simulation {
     // The time to wait between each action - based on student Id last digit -> 7
     private int thinkTime = 7;
 
-    private int randomLowerBound = 1;
-    private int randomUpperBound = 1000;
+    private int itemsAmt = Integer.parseInt(System.getProperty("itemsAmt", "6"));
 
-    private int randomItemId = ThreadLocalRandom.current().nextInt(randomLowerBound, randomUpperBound);
 
     // A. Browse the items
     ChainBuilder browse = exec(
         http("Browse Items")
-            .get("/products"),
-            pause(1),
+            .get("/products")
             .check(status().is(200))
+            .check(jsonPath("$[0].id").exists().saveAs("randomItemId"))
+            .check(header("JSESSIONID").saveAs("sessionId"))
     );
 
     // B. Add a random item to the cart (CONFIGURABLE by randomItemId)
-    ChainBuilder addRandomToCart = exec();
+    /*ChainBuilder addRandomToCart = exec(
+        http("Add an amount of random items to Cart")
+            .post("/cart/#{randomItemId}")  // Use the extracted value from browse
+            .body(StringBody("{ \"id\": \"#{randomItemId}\", \"content\": { \"key\": 1 }, \"creationDate\": \"2024-11-16T00:00:00Z\" }"))
+            .asJson()
+            .check(status().is(201))
+    );*/
+
+    ChainBuilder addRandomToCart = repeat(itemsAmt).on(
+        exec(
+            http("Add an amount of random items to Cart")
+                .post("/cart/#{randomItemId}")  // Use the extracted value from browse
+                .body(StringBody("{ \"id\": \"#{randomItemId}\", \"content\": { \"key\": 1 }, \"creationDate\": \"2024-11-16T00:00:00Z\" }"))
+                .asJson()
+                .check(status().is(201))
+        )
+    );
 
     // C. Confirm the cart
-    ChainBuilder confirm = exec();
+    ChainBuilder confirm = exec(
+        http("Confirm Cart")
+            .post("/confirm")
+            .body(StringBody( "{ " +
+            "\"cardONumber\": \"1234567890123456\", " +
+            "\"cardOwner\": \"John\", " +
+            "\"checksum\": \"123\", " +
+            "\"sessionId\": \"#{sessionId}\", " +
+            "\"cardType\": \"MasterCard\", " +
+            "\"cardOwnerLastname\": \"Hernandez\", " +
+            "\"cardOwnerAddress\": \"Stuttgart\"" +	
+            "}"))
+            .asJson()
+            .check(status().is(200))
+    );
 
     // ~. Think -> placed somewhere in the middle of the scenario
-    ChainBuilder think = exec(pause(thinkTime));
+    ChainBuilder think = exec(pause(java.time.Duration.ofSeconds(thinkTime)));
 
-    HttpProtocolBuilder httpProtocol = http.baseUrl("http://localhost:8081");
-
+    HttpProtocolBuilder httpProtocol = http
+    .baseUrl("http://localhost:8081")
+    .acceptHeader("application/json")
+    .contentTypeHeader("application/json")
+    .userAgentHeader("Gatling/StressTest")
+    .inferHtmlResources();
 
     ScenarioBuilder stressTest = scenario("Stress Test")
-        .exec(browse)
-        .exec(think)
-        .exec(addRandomToCart)
-        .exec(confirm);
+    .exec(browse)
+    .exec(think)
+    .exec(addRandomToCart)
+    .exec(think)
+    .exec(confirm);
+    
+    /*ScenarioBuilder stressTestInOnemethod = scenario("Stress Test II")
+    .exec(
+        http("Browse Items")
+            .get("/products")
+            .check(status().is(200))
+    )
+    .pause(java.time.Duration.ofSeconds(thinkTime))
+
+    .exec(
+        http("Add an amount of random items to Cart")
+        .post("/cart/add")
+        .formParam("id", String.valueOf(randomItemId))
+        .check(status().is(200))
+    )
+    .pause(java.time.Duration.ofSeconds(thinkTime))
+
+    .exec(
+        http("Confirm Cart")
+            .get("/cart/confirm")
+            .check(status().is(200))
+    );*/
 
     {
         setUp(
-            stressTest.inject(constantUsersPerSec(10).during(60))
-        ).protocol(httpProtocol);
+            stressTest.injectOpen(
+                constantUsersPerSec(10).during(60).randomized()
+            ).protocols(httpProtocol)
+        );
     }
 }
